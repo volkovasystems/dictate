e1b1934cec552cb0f0e6b6d8f9b33c0eea029722
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-04 09:58:36 +0800

    Updated test.

--- dictate-test.js
+++ dictate-test.js
@@ -1,6 +1,8 @@
 "use strict";
 
+const assert = require( "assert" );
 const dictate = require( "./dictate.js" );
 
-console.log( dictate( [ 1, 2 ], [ 2, 1 ] ) );
-console.log( dictate( ) );
+assert.deepEqual( dictate( [ 1, 2 ], [ 2, 1 ] ), [ 2, 1 ] );
+
+console.log( "ok" );
