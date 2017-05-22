64f502e5828aca5774e63b47e4843d346f009401
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-22 13:39:25 +0800

    Updated test.

--- dictate-test.js
+++ dictate-test.js
@@ -2,10 +2,6 @@
 const assert = require( "assert" );
 const dictate = require( "./dictate.js" );
 
-assert.deepEqual( dictate( [ Array, Object, RegExp, Date ], {
-	"RegExp": 0,
-	"Array": 1,
-	"Object": 2,
-} ), [ RegExp, Array, Object, Date ], "should follow order" );
+assert.deepEqual( dictate( [ Array, Object, RegExp, Date ], { "RegExp": 0, "Array": 1, "Object": 2 } ), [ RegExp, Array, Object, Date ], "should follow order" );
 
 console.log( "ok" );
