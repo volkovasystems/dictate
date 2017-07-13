4b3579555aefb70ede2531fe00a8d228e0c1d9a8
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-13 16:02:28 +0800

    Updated server/client test.

--- dictate-test.js
+++ dictate-test.js
@@ -2,9 +2,6 @@
 const assert = require( "assert" );
 const dictate = require( "./dictate.js" );
 
-assert.equal( Array.isArray( dictate( [ Array, Object, RegExp, Date ], { "RegExp": 0, "Array": 1, "Object": 2 } ) ),
-				true, "should return true" );
-
 assert.deepEqual( dictate( [ 1, 2, 3, 4, 5 ], [ 5, 4, 3, 2, 1 ] ),
 					[ 5, 4, 3, 2, 1 ], "should follow order" );
 
