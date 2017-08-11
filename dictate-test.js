5196ef796643030fbdcf33c1eb6a27730ee98cde
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-11 17:36:11 +0800

    Updated test.

--- dictate-test.js
+++ dictate-test.js
@@ -3,6 +3,6 @@ const assert = require( "assert" );
 const dictate = require( "./dictate.js" );
 
 assert.deepEqual( dictate( [ 1, 2, 3, 4, 5 ], [ 5, 4, 3, 2, 1 ] ),
-					[ 5, 4, 3, 2, 1 ], "should follow order" );
+	[ 5, 4, 3, 2, 1 ], "should be equal to [ 5, 4, 3, 2, 1 ]" );
 
 console.log( "ok" );
